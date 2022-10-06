ccbf6c4d0f1313eb39bb17945b13a362be7b53c9
laxman <laxman091@bitbucket.org>
2022-10-06 10:34:57 +0530

    first commit

---
+++ src/About - Copy - Copy.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function About() {
+  return (
+    <div className="App1">
+      About  hello about Component Here
+	  
+    </div>
+  );
+}
+
+export default About;
